4562d88b09deb85dfad86f25ec14fcb993b4474f
Sials Detering <silas.detering@tu-dortmund.de>
2023-12-13 11:50:44 +0100

    Fix route handlers in app.js (test)

--- src/server/app.js
+++ src/server/app.js
@@ -84,11 +84,11 @@ app.use('/reports/iron', esvReportRoutesIron);
 app.use('/reports/steel', esvReportRoutesSteel);
 
 // Index Route
-app.get('/', (res) => {
+app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
-app.get('*', (res) => {
-    res.send(path.join(__dirname, 'public/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // ==========================================================================================
