22b3d8c97a6bcdaaed302001ec547d554944a335
Silas <101791439+SilasDetering@users.noreply.github.com>
2023-12-19 15:50:29 +0100

    deploy frontend test v8

--- src/server/app.js
+++ src/server/app.js
@@ -84,8 +84,8 @@ app.use('/reports/iron', esvReportRoutesIron);
 app.use('/reports/steel', esvReportRoutesSteel);
 
 // Index Route
-app.get('/', (req,res) => {
-    res.sendFile(process.cwd()+"/esv_prod_test/dist/esv_webapp_v2/browser/index.html")
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('*', (req, res) => {
